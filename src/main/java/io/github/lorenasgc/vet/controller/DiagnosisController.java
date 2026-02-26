8f747419135507090fa4dad2a17229877e818c1b
Lorena González <lorenascg@gmail.com>
2026-02-26 11:52:25 +0100

    Refactor: Use constructor injection for dependencies

--- src/main/java/io/github/lorenasgc/vet/controller/DiagnosisController.java
+++ src/main/java/io/github/lorenasgc/vet/controller/DiagnosisController.java
@@ -1,22 +1,23 @@
 package io.github.lorenasgc.vet.controller;
 
 import io.github.lorenasgc.vet.dto.DiagnosisDTO;
-import io.github.lorenasgc.vet.service.PetService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+import io.github.lorenasgc.vet.service.PetService;
 
 import java.util.Set;
 
 @RestController
 @RequestMapping("/api/diagnoses")
+@RequiredArgsConstructor
 public class DiagnosisController {
 
-    @Autowired
-    private PetService petService;
+    private final PetService petService;
 
     @GetMapping("/species/{speciesName}")
     public ResponseEntity<Set<DiagnosisDTO>> getDiagnosesBySpecies(@PathVariable String speciesName) {
